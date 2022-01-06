298aec5a92b70e859c21f5f78c4d78c3d194ace9
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2022-01-06 21:58:40 +0100

    first is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+// console.log(refs.startBtn);
+let timerId = null;
 
+const changeBodyColor = function () {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.bodyColor.style.backgroundColor = color;
+  }, 1000);
+};
+
+refs.startBtn.addEventListener('click', changeBodyColor);
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
