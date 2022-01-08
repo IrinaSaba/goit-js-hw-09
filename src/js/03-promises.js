d3780a5d65bec14e757c67c14b660a4c34ef17ce
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2022-01-08 01:26:09 +0100

    third is under construction

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelectorAll('input')[0],
+  step: document.querySelectorAll('input')[1],
+  amount: document.querySelectorAll('input')[2],
+  submit: document.querySelector('button'),
+};
+
+refs.submit.addEventListener('click', createPromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      // console.log(shouldResolve);
+
+      if (shouldResolve) {
+        resolve(` Fulfilled promise ${position} in ${delay}ms`); // Fulfill
+      }
+      reject(` Rejected promise ${position} in ${delay}ms`); // Reject
+    }, '${delay}');
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
